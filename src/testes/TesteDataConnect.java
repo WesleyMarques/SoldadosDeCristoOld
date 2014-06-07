3835d7148b3ac67d409389f4e2378b00d7697006
WesleyMarques <wesley.torres@ccc.ufcg.edu.br>
2014-06-07 17:40:34 -0300

    Alteração nos arquivos de controle

--- src/testes/TesteDataConnect.java
+++ src/testes/TesteDataConnect.java
@@ -20,5 +20,4 @@ public class TesteDataConnect {
 		System.out.println(connection.getConn().isClosed());
 		
 	}
-
 }
--- src/testes/testaData.java
+++ src/testes/testaData.java
@@ -1,19 +1,17 @@
 package testes;
 
 import static org.junit.Assert.*;
-
-import java.util.List;
-
+import java.io.File;
+import org.junit.Assert;
 import org.junit.Test;
-import org.sc.codes.User;
-import org.sc.data.DataOffline;
 
 public class testaData {
-	
+
 	@Test
-	public void deveCarregarArquivo(){
-		List<User> listaUsers = DataOffline.loadUsersLogin();
-		assertFalse(listaUsers.isEmpty());
+	public void Deve() {
+		
+		
+
 	}
 
 }
